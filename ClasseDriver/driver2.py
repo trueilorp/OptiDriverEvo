cbbab3b60a85413f241d7ded1516ed975a3dbfee
trueilorp <102355477+trueilorp@users.noreply.github.com>
2023-09-15 17:43:45 +0200

    Add files via upload

--- ClasseDriver/driver2.py
+++ ClasseDriver/driver2.py
@@ -37,15 +37,4 @@ class Driver:
 
         return listAttributi
 
-    def mutateDriver (self):
-        listAttributi = self.creaListaParametriDriver()
-        parametroDaModificare = random.choice(listAttributi)
-        
-        valoreDiModifica = random.uniform(-0.1, 0.1)#scelgo un valore di modifica che sta tra 0,01 e 0,1
-        parametroDaModificare = parametroDaModificare + valoreDiModifica #modifico il parametro
-        
-        #controlla se la mutazione mi va bene 
-        #if valoreDiModifica == 0 or parametroDaModificare > 1 or parametroDaModificare < 0:
-            #rifai mutazione 
-        print(listAttributi)
 
