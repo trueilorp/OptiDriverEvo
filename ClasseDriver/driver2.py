5958d30b44778fb392471a1e8bc4239f89426f33
trueilorp <102355477+trueilorp@users.noreply.github.com>
2023-09-26 17:43:39 +0200

    Add files via upload

--- ClasseDriver/driver2.py
+++ ClasseDriver/driver2.py
@@ -5,7 +5,7 @@ class Driver:
 
     #driverConfig = np.array([0,0,0,0,0,0,0,0,0,0,0,0])
 
-    WIDTHLANE = 14
+    WIDTHLANE = 3.500 #ho verificato dallo scenario editor 
 
     def __init__(self, lg):
        self.listaGeni = lg
@@ -15,16 +15,21 @@ class Driver:
     def setGeni(self, lista):
         self.listaGeni = lista
 
-    def computeLaneOffset (self, dist):
-        self.LaneOffset = (self.WIDTHLANE/2) + abs (dist)
-    
     def setLaneOffset(self, lo):
         self.LaneOffset = lo
 
     def setTimePath(self, tp):
          self.timePath = tp
+
+# Controllo se va fuori strada andando a calcolarmi la meta' della strada e poi vedo se il check e' minore o maggiore di 0
+    def computeLaneOffset (self, laneOffsetTimeStamp):
+        check = (self.WIDTHLANE/2) - abs (laneOffsetTimeStamp)
+        if check < 0:
+            return 0 # vuol dire che e' fuoristrada
+        else:
+            print("apposto")
         
-    def creaListaParametriDriver(self):
+    """def creaListaParametriDriver(self):
         listAttributi = [self.DesiredVelocity,self.DesiderAcceleration, self.DesiderDeceleration,
         self.CurveSpeed, self.ObserveSpeedLimit ,self.DistanceKeeping,
         self.LaneKeeping, self.SpeedKeeping, self.LaneChangeDynamic, self.UrgeToOvertake,
@@ -35,6 +40,6 @@ class Driver:
         self.UseOfIndicator ,
         self.ReactionTime,]
 
-        return listAttributi
+        return listAttributi"""
 
 
